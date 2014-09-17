4b4b0c24d242a354aa52251f093581d74ba03d75
Rohit <rgarg1992@gmail.com>
2014-09-17 11:41:10 +0530

    add new Url for list display

--- lists/views.py
+++ lists/views.py
@@ -8,8 +8,7 @@ def home_page(request):
    	new_item = Item.objects.create(text=new_item_text)
 	return redirect('/lists/the-only-list')
 
-    items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'home.html')
 
 def view_list(request):
     items = Item.objects.all()
